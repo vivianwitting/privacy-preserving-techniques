99e7750e8417ef79cf4fb50b4952f3b92c36e413
vivianwitting <55049434+vivianwitting@users.noreply.github.com>
2023-06-09 19:39:40 +0200

    Add files via upload

---
+++ microservice1.py
@@ -0,0 +1,138 @@
+
+import os
+import json
+import pydicom
+import numpy as np
+from Pyfhel import Pyfhel
+from flask import Flask, jsonify
+
+noise_array = np.random.randint(0, 20, size=30)
+data_folder = "images50"
+epsilon = 0.8 # Privacy parameter
+sensitivity = 1.0
+delta = 10e-4
+
+app = Flask(__name__)
+
+bfv_params = {
+    'scheme': 'BFV',    # can also be 'bfv'
+    'n': 2**15,         # Polynomial modulus degree, the num. of slots per plaintext,
+                        #  of elements to be encoded in a single ciphertext in a
+                        #  2 by n/2 rectangular matrix (mind this shape for rotations!)
+                        #  Typ. 2^D for D in [10, 16]
+    't': 65537,         # Plaintext modulus. Encrypted operations happen modulo t
+                        #  Must be prime such that t-1 be divisible by 2^N.
+    't_bits': 20,       # Number of bits in t. Used to generate a suitable value
+                        #  for t. Overrides t if specified.
+    'sec': 128,         # Security parameter. The equivalent length of AES key in bits.
+                        #  Sets the ciphertext modulus q, can be one of {128, 192, 256}
+                        #  More means more security but also slower computation.
+}
+app = Flask(__name__)
+
+
+# Differential privacy
+def add_laplace_noise(image, epsilon):
+    shape = image.shape
+    noise = np.random.laplace(scale=sensitivity/epsilon, size=shape)
+    noisy_data = np.round(image + noise).astype(int)
+    return noisy_data
+
+# Differential privacy
+def add_gaussian_blur(data, delta):
+    # Compute the standard deviation (sigma) based on epsilon and sensitivity
+    sigma = (2 * (sensitivity**2) * np.log(1.25/delta)) / (epsilon**2)
+    noisy_data = data + np.random.normal(0, sigma, size=data.shape)
+    return noisy_data
+
+# Homomorphic encryption
+def homomorphic_encryption(pixel_array, bfv_params):
+    HE = Pyfhel()           # Creating empty Pyfhel object
+    HE.contextGen(**bfv_params)  # Generate context for bfv scheme
+    HE.keyGen()             # Key Generation: generates a pair of public/secret keys
+    HE.relinKeyGen()        # Relinearization key generation
+
+    # Resize the image to fit the target number of slots
+    ptxt = HE.encodeInt(pixel_array)
+    ctxt = HE.encryptPtxt(ptxt)
+
+    # cc_mull = ctxt * ctxt
+    # cc_sum = cc_mull + cc_mull
+
+    d_result = HE.decryptInt(ctxt)
+
+    return json.dumps(d_result.tolist())
+
+@app.route('/encrypted_results', methods=['GET'])
+def encrypted_results():
+    # Retrieve all DICOM image files in the "images" folder
+    image_names = os.listdir(data_folder)
+    processed_images = []
+
+    for image_name in image_names:
+        image_path = os.path.join(data_folder, image_name)
+        image = pydicom.dcmread(image_path)
+        converted_array = image.pixel_array.flatten()[:bfv_params['n']].astype(np.int64)
+        cM = homomorphic_encryption(converted_array, bfv_params)
+        processed_images.append(cM)
+
+    return jsonify(processed_images)
+
+@app.route('/plain_results', methods=['GET'])
+def plain_results():
+    # Retrieve all DICOM image files in the "images" folder
+    image_names = os.listdir(data_folder)
+    processed_images = []
+
+    for image_name in image_names:
+        image_path = os.path.join(data_folder, image_name)
+        image = pydicom.dcmread(image_path)
+        converted_array = image.pixel_array.flatten()[:bfv_params['n']].astype(np.int64)
+        # acMul = converted_array * converted_array
+        # acSum = acMul + acMul
+        processed_images.append(converted_array.tolist())
+
+    return jsonify(processed_images)
+
+@app.route('/processed_images', methods=['GET'])
+def processed_images():
+    # Retrieve all DICOM image files in the "images" folder
+    image_names = os.listdir(data_folder)
+    processed_images = []
+
+    for image_name in image_names:
+        image_path = os.path.join(data_folder, image_name)
+        image = pydicom.dcmread(image_path)
+        image = image.pixel_array
+
+        # Apply differential privacy to each image using Laplace noise
+        # proc_image = add_laplace_noise(image, epsilon)
+        proc_image = add_gaussian_blur(image, delta)
+
+        # Changing every pixel as way of anonymizing
+        # proc_image = anonymize_image(image)
+
+
+        processed_images.append(proc_image.tolist())
+
+    return jsonify(processed_images)
+
+
+@app.route('/original_images', methods=['GET'])
+def original_images():
+    # Retrieve all DICOM image files in the "images" folder
+    image_names = os.listdir(data_folder)
+    original_images = []
+
+    for image_name in image_names:
+        image_path = os.path.join(data_folder, image_name)
+        image = pydicom.dcmread(image_path)
+
+        original_images.append(image.pixel_array.tolist())
+
+    return jsonify(original_images)
+
+# Example usage:
+# Start the Flask app
+if __name__ == '__main__':
+    app.run(port=5000)
---
+++ microservice2.py
@@ -0,0 +1,139 @@
+import time
+import psutil
+import requests
+import os
+import pydicom
+import ast
+import sys
+import numpy as np
+from flask import Flask, jsonify
+from skimage.metrics import structural_similarity as ssim
+
+app = Flask(__name__)
+
+def compute_loss(original_data, private_data):
+    mse = np.mean((original_data.astype(float) - private_data.astype(float)) ** 2)
+    ssims = ssim(original_data.astype(float), private_data.astype(float))
+    return mse, ssims
+
+@app.route('/evaluate_result', methods=['GET'])
+def evaluate_result():
+    cpu_usage_before = psutil.cpu_percent()
+    evaluation_results = []
+
+    response = requests.get("http://127.0.0.1:5000/plain_results")
+    plain_results = response.json()
+    start_time = time.time()
+
+    response = requests.get("http://127.0.0.1:5000/encrypted_results")
+    encrypted_results = response.json()
+    cpu_usage_after = psutil.cpu_percent()
+
+    end_time = time.time()
+
+    processing_time = end_time - start_time
+    encrypted_results = ast.literal_eval(encrypted_results[0])
+
+    # mse, ssims = compute_loss(np.array(plain_results[0]), np.array(encrypted_results))
+
+    evaluation_results.append({
+        'processing_time': processing_time,
+        'cpu_usage_after': cpu_usage_after,
+        # 'similarity (-1,1)': ssims,
+        'size of original': sys.getsizeof(plain_results),
+        'size of decrypt': sys.getsizeof(encrypted_results)
+        # 'mse': mse
+    })
+
+    return jsonify(evaluation_results)
+
+@app.route('/second_party', methods=['GET'])
+def add_to_first():
+    response = requests.get("http://127.0.0.1:5000/original_images")
+    input_of_two = response.json()
+    response = requests.get("http://127.0.0.1:5000/processed_images")
+    retrieved = response.json()
+
+    second = retrieved + input_of_two
+
+    return jsonify(second)
+
+@app.route('/get_secure_sum', methods=['GET'])
+def get_smc():
+    response = requests.get("http://127.0.0.1:5000/secure_sum")
+    retrieved = response.json()
+
+    return jsonify(retrieved)
+
+
+@app.route('/evaluate_data', methods=['GET'])
+def evaluate_data():
+    # Perform utility and efficiency evaluation for each image
+    evaluation_results = []
+
+    response = requests.get("http://127.0.0.1:5000/original_images")
+    original_images = response.json()
+    base_time = time.time()
+    cpu_usage_before = psutil.cpu_percent()
+
+    response = requests.get("http://127.0.0.1:5000/processed_images")
+    processed_images = response.json()
+
+    cpu_usage_after = psutil.cpu_percent()
+    end_time = time.time()
+
+    processing_time = end_time - base_time
+
+    # mse, ssims = compute_loss(np.array(original_images), np.array(processed_images))
+
+    evaluation_results.append({
+        'processing_time': processing_time,
+        'cpu_usage_after': cpu_usage_after,
+        # 'similarity (-1,1)': ssims,
+        # 'size of original': sys.getsizeof(original_images),
+        # 'size of processed': sys.getsizeof(processed_images),
+        # 'mse': mse
+    })
+
+    return jsonify(evaluation_results)
+
+
+# @app.route('/result', methods=['POST'])
+# def receive_result():
+#     result = request.json['result']
+
+#     # Process the result or perform any desired logic
+#     print('Received result:', result)
+
+#     return 'Result received successfully'
+
+@app.route('/original_images', methods=['GET'])
+def original_images():
+    data_folder = "images2"
+    # Retrieve all DICOM image files in the "images" folder
+    image_names = os.listdir(data_folder)
+    original_images = []
+
+    for image_name in image_names:
+        image_path = os.path.join(data_folder, image_name)
+        image = pydicom.dcmread(image_path)
+
+        original_images.append(image.pixel_array.tolist())
+
+    return jsonify(original_images)
+
+# Start the Flask app
+if __name__ == '__main__':
+    app.run(port=5010)
+
+# @app.route("/receive", methods=["POST"])
+# def receive():
+#     # Get the shared result from the request
+#     result = MPCTensor(session=session, shares=request.json["shares"])
+
+#     # Reconstruct the result by combining the shares
+#     result.reconstruct()
+
+#     # Perform further computations on the result or use it as needed
+
+#     return "Processing complete"
